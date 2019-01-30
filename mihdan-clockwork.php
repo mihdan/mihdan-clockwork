ed0a9f1102664b71881b6fa04add0cfc8c0611b0
Mikhail Kobzarev <mihdan@yandex.ru>
2019-01-30 21:05:37 +0300

    Инициализация плагина

---
+++ mihdan-clockwork.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Mihdan Clockwork
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     mihdan-clockwork
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Mihdan_Clockwork
+ */
+
+// Your code starts here.
